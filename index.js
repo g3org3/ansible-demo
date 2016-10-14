ff3a910db0b1cc12a3338aca6201c09754b827b6
g3org3 <7jagjag@gmail.com>
2016-10-14 09:50:14 -0600

    set port 3000

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
-})
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
