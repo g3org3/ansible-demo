94580fc73b827730c08c716c9dae76ab472cc956
g3org3 <7jagjag@gmail.com>
2016-10-14 10:33:37 -0600

    change output

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
-
+var exec = require('child_process').exec;
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World! served from: '+req.ip);
+  exec('curl ipinfo.io', (err, info) => {
+    res.send('<pre style="font-size:30px;">Hello World! served from: '+JSON.parse(info).ip);
+  })
 });
 
 app.listen(3000, function () {
