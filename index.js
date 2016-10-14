75763e57d62dae2da02d9a05e9526a848957814e
g3org3 <7jagjag@gmail.com>
2016-10-14 10:40:55 -0600

    change ip

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
-var exec = require('child_process').exec;
+
 app.get('/', function (req, res) {
-  exec('curl ipinfo.io', (err, info) => {
-    res.send('<pre style="font-size:30px;">Hello World! served from: '+JSON.parse(info).ip);
-  })
+  var ip = server.address().address == '::'? 'localhost': server.address().address;
+  res.send('<pre style="font-size:30px;">Hello World! served from: '+ip);
 });
 
-app.listen(3000, function () {
+var server = app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
