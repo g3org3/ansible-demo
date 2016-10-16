1723daaffb4747022bf2fa82e4122e8064ae8990
g3org3 <7jagjag@gmail.com>
2016-10-15 21:24:03 -0600

    get hostname

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-var express = require('express');
+var express = require('express')
 var uuid = require('uuid')
+var exec = require('child_process').exec
 var app = express();
 var ID = uuid.v1();
 
 var redis = require("redis"),
     client = redis.createClient();
 
-app.get('/', function (req, res) {
-  client.get("server:"+ID, function(err, count) {
-    count = count || '0'
-    count = Number(count) + 1
-    client.set("server:"+ID, ""+count+"")
-    res.send('<pre style="font-size:30px;">Hello World!\nserved from: '+ID.substr(19, 9) +'\nserved: '+count+' times');
-  });
+exec('hostname', (err, out) => {
   
-});
+  app.get('/', function (req, res) {
+    client.get("server:"+ID, function(err, count) {
+      count = count || '0'
+      count = Number(count) + 1
+      client.set("server:"+ID, ""+count+"")
+      res.send('<pre style="font-size:30px;">Hello World!\nserved from: '+out +'\nserved: '+count+' times');
+    });
+    
+  });
 
-var server = app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+  var server = app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
+
+})
